0120a762ab60108bb39d8ecbcb2d18680c5e6667
Luke Boyett <lboyett@gmail.com>
2023-01-05 22:39:38 +0100

    Updates

--- src/Calendar.js
+++
@@ -1,9 +0,0 @@
-const Calendar = () => {
-  return (
-    <div className="body-div" id="about-body">
-      <h1>Calendar</h1>
-    </div>
-  );
-};
-
-export default Calendar;
--- src/Contact.js
+++
@@ -1,9 +0,0 @@
-const Contact = () => {
-  return (
-    <div className="body-div" id="about-body">
-      <h1>Contact</h1>
-    </div>
-  );
-};
-
-export default Contact;
